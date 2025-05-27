63c19aca70a7bc6f222b8b43a7d639bddf9f67cf
Ubuntu <ubuntu@ip-172-31-33-200.eu-north-1.compute.internal>
2025-05-27 11:35:04 +0000

    added range endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from task import predict_rank
+from get_range import predict_rank_range
 
 app = Flask(__name__)
 
@@ -22,6 +23,23 @@ def calculate():
 
     result = predict_rank(student_marks, easy_questions, medium_questions, hard_questions)
     return jsonify({"predicted_rank": result})
+
+@app.route('/get-rank-range', methods=['POST'])
+def get_rank_range():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid input, no JSON data provided."}), 400
+
+    try:
+        marks = data['marks']
+    except KeyError as e:
+        return jsonify({"error": f"Missing field: {str(e)}"}), 400
+
+    result = predict_rank_range(marks)
+    if(len(result) == 2):
+        return jsonify({"min_rank": result[0], "max_rank": result[1]})
+    else:
+        return jsonify({"max_rank": result})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5002)
